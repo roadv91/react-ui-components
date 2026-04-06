d630d01f59e98785048ba9bbf3c51148b2bd07aa
RoAdv <roadventures91@gmail.com>
2026-04-05 17:48:00 -0700

    separated tsconfigs for node and app and updated es version for targets

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default tseslint.config(
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2023, // Should match `target` in tsconfigs.
       globals: globals.browser,
     },
     plugins: {
