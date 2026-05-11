a1416a3d8b907ea9704e19b40fca5968a28d02d8
roadv <roadventures91@gmail.com>
2026-05-10 18:14:36 -0700

    Created Button component (#6)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import './styles/breakpoints.scss'
 
 // Public API — export all components from here
 export { Menu, type MenuOptions } from '@/components/menu/menu'
+export { Button, type ButtonProps } from '@/components/button/button'
